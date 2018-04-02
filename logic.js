c745fc87a5dd5580db74351fada0aa3097225c17
Mary Quint <mary.k.quint@gmail.com>
2018-04-01 22:32:26 -0500

    removing trains working

--- logic.js
+++ logic.js
@@ -12,58 +12,67 @@ var config = {
 
   var now = moment().format('hh:mm');
   console.log(now)
+  var childNumber = 0;
 
 $("#submit-button").on("click", function () {
-
-    // event.preventDefault();
     
+
     let trainName = $("#inputName").val()
     let destination = $("#inputDestination").val()
     let firstTime = $("#inputStartTime").val()
     let frequency = $("#inputFrequency").val()
-
-
-    // let minutesAway = nextArrival - now;
     
-
-
-
-
-
-
-
-    
-
-   console.log(trainName)
-   console.log(destination) 
+    console.log(trainName)
+    console.log(destination) 
 
     database.ref().push({
         name: trainName,
         destination: destination,
         firstTrain: firstTime,
         frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP       
+    });  
 
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-        
-    });
-    
+
 })
 
 
 database.ref().on("child_added", function(snapshot){
     
-    let newTR = $("<tr>")
+console.log(snapshot.key)
+
+    childNumber ++
+    var firstTimeConverted = moment(snapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+    
+    var currentTime = moment();
+    
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    
+    var tRemainder = diffTime % snapshot.val().frequency;
+    
+    var tMinutesTillTrain = snapshot.val().frequency - tRemainder;
+    
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    
+    let newTR = $("<tr>").attr("id", childNumber)
     
     let newTrain = $("<td>").text(snapshot.val().name)
     let newDestination = $("<td>").text(snapshot.val().destination)
-    let newStartTime = $("<td>").text(snapshot.val().firstTrain)
-    // let newPayRate = $("<td>").text(snapshot.val().rate)
+    let newFrequency = $("<td>").text(snapshot.val().frequency)
+    let nextArrival = $("<td>").text(moment(nextTrain).format("h:mm"))
+    let minutes = $("<td>").text(tMinutesTillTrain)
+    let removeButton = $("<button>").addClass("remove btn btn-primary").text("Remove train")
+    removeButton.attr("data-childkey", snapshot.key)
+    removeButton.attr("data-order", childNumber)
 
-    newTR.append(newTrain)
-    newTR.append(newDestination)
-    newTR.append(newStartTime)
-    newTR.append("<td class='next-arrival'>")
-    newTR.append("<td class='minutes-away'>")
+    newTR.append(newTrain, newDestination, newFrequency, nextArrival, minutes, removeButton)
     $("#table-body").append(newTR)
 
+})
+
+$("#table-body").on("click", "button", function() {
+    var childKey = $(this).attr("data-childkey")
+    var rowNumber = $(this).attr("data-order")
+    database.ref().child(childKey).remove()
+    $(`#${rowNumber}`).empty()
 })
